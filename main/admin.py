84f7050f69ca6afebc70e888c9128c8ec06aa477
Mohamork <k.m19981102@gmail.com>
2026-01-08 06:49:55 +0100

    Update admin.py
    Create project admin
    Create ImageInline field
    Create tag admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Project, Image, Tag
 
-# Register your models here.
+# -- Create the ImageInline field
+class ProjectImageInline(admin.TabularInline) :
+    model = Image
+    extra = 1
+
+# -- Create the projectadmin
+class ProjectAdmin(admin.ModelAdmin) :
+    list_display = (
+        'title',
+    )
+    inLines = [ProjectImageInline]
+    search_fields = (
+        'title',
+        'description'
+    )
+    list_filter = ('tags',)
+
+# -- Create the tagadmin
+class TagAdmin(admin.ModelAdmin) : 
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+# -- Register the models
+admin.site.register(Tag,TagAdmin)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Image)
