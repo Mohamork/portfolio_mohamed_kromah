05689be81ae689439ea6ab6e00c0feaf3259c875
Mohamork <k.m19981102@gmail.com>
2026-01-12 06:48:39 +0100

    Update urls.py
    - connect urls to views.

--- main/urls.py
+++ main/urls.py
@@ -1,1 +1,9 @@
-url_patterns = []
+from django.urls import path
+from . import views
+
+url_patterns = [
+    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('project/<int:id>',views.project,name='project'),
+]
