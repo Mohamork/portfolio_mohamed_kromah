816ef8ff4504bc0b54e2f7f807a72f491c415f5f
Mohamork <k.m19981102@gmail.com>
2026-01-16 00:42:25 +0100

    Summary
    add indentations to home.html
    fix spelling in home.js
    fix spelling in home.html

--- main/static/js/home.js
+++ main/static/js/home.js
@@ -1,4 +1,4 @@
-document.AddEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const nameSearch = document.getElementById("name-search")
     const tags = document.querySelectorAll(".tag")
     const projects = document.querySelectorAll(".project")
@@ -7,17 +7,19 @@ document.AddEventListener("DOMContentLoaded", function () {
         const nameQuery = nameSearch.value.toLowerCase() ;
 
         projects.forEach((project) => {
-            const name = project.getAttribute('data-name') ;
+            const name = project.getAttribute('data-name')
             const nameMatch = name.includes(nameQuery)
 
             if (nameMatch) {
-                project.style.display = "";      }
-            else {
-                project.style.display = 'none' }
+                project.style.display = "";      
+            }else {
+                project.style.display = "none";
+            }
         
-            })
+        })
 
-        }
+    }
 
-        nameSearch.addEventListener("keyup",filterProjects)
-} )
+    nameSearch.addEventListener("keyup",filterProjects)
+
+})
