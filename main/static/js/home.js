c48e522c11d19389969667957effd062b11b700a
Mohamork <k.m19981102@gmail.com>
2026-01-16 00:00:32 +0100

    Update home.js
    add functions for searching and filtering on projects

---
+++ main/static/js/home.js
@@ -0,0 +1,22 @@
+document.AddEventListener("DOMContentLoaded", function () {
+    const nameSearch = document.getElementById("name-search")
+    const tags = document.querySelectorAll(".tag")
+    const projects = document.querySelectorAll(".project")
+
+    function filterProjects() {
+        const nameQuery = nameSearch.ariaValueMax.toLowerCase() ;
+
+        projects.forEach((project) => {
+            const name = project.getAttribute('data-name') ;
+            const nameMatch = name.includes(nameQuery)
+
+            if (nameMatch) {
+                project.style.display = "";      }
+            else {
+                project.style.display = 'none' }
+        
+            })
+
+        }
+        
+} )
