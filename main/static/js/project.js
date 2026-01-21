31fdbce87bfc1de1c79d36cde69ca472f6afc7fa
Mohamork <k.m19981102@gmail.com>
2026-01-21 07:17:33 +0100

    Write functions: Show image carousel

---
+++ main/static/js/project.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+showSlides(slideIndex)
+
+function moveSlide(n) {
+    slideIndex += n
+    showSlides((slideIndex += n))
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item")
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.lenght;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none'
+    }
+    slides[slideIndex - 1].style.display = 'flex'
+
+}
