60a11da59d3f9254293aadd4a5d3f130f1a8f9c0
Mohamork <k.m19981102@gmail.com>
2026-01-28 14:43:43 +0100

    Add db

--- portfolio_mohamed_kromah/settings.py
+++ portfolio_mohamed_kromah/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +77,10 @@ WSGI_APPLICATION = 'portfolio_mohamed_kromah.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
