0a324df42bbb69db2ba03a84d4343a9a472349ad
Mohamork <k.m19981102@gmail.com>
2026-01-28 15:34:10 +0100

    Use WhiteNoise to server staticfiles for production

--- portfolio_mohamed_kromah/settings.py
+++ portfolio_mohamed_kromah/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Custom middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio_mohamed_kromah.urls'
@@ -120,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
